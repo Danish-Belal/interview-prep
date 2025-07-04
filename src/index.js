7084ba3d1c5935afac761ed6eebdeb91833ccfd5
Danish-Belal <danishexplore019@gmail.com>
2025-07-04 19:50:32 +0530

    adding DocerFIle

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
 const express = require('express');
 const app = new express();
 // require('./schedular1');
-require('./schedular2');
+// require('./schedular2');
 app.use(express.json());
 
+
+app.get('/', (req,res)=>{
+    res.send("Hello fron index, tell me what you want")
+})
 app.listen(5000,()=>{
     console.log('APP is runiing on port 5000');
     
